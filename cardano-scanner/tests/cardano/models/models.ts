24e6ea8aca800faaabd85e8b58d6c386cf2ca5c2
sahandzou <sahandzoufan79@gmail.com>
2022-07-02 19:47:21 +0430

    adding cardano tests

---
+++ cardano-scanner/tests/cardano/models/models.ts
@@ -0,0 +1,122 @@
+import { DataSource } from "typeorm";
+import { expect } from "chai";
+import { entities } from "../../../lib/entities";
+import { migrations } from "../../../lib/migrations";
+import { NetworkDataBase } from "../../../lib/models/networkModel";
+import { Observation } from "../../../lib/objects/interfaces";
+
+export const loadDataBase = async (name: string): Promise<NetworkDataBase> => {
+    const ormConfig = new DataSource({
+        type: "sqlite",
+        database: `./sqlite/watcher-test-${name}.sqlite`,
+        entities: entities,
+        synchronize: false,
+        migrations: migrations,
+        logging: false,
+    });
+    return await NetworkDataBase.init(ormConfig);
+}
+
+export const firstObservations: Array<Observation> = [{
+    fromChain: "erg",
+    toChain: "cardano",
+    fromAddress: "ErgoAddress",
+    toAddress: "cardanoAddress",
+    amount: "1000000000",
+    fee: "1000000",
+    sourceChainTokenId: "ergoTokenId",
+    targetChainTokenId: "cardanoTokenId",
+    sourceTxId: "ergoTxId1",
+    sourceBlockId: "ergoBlockId",
+    requestId: "reqId1",
+}];
+
+export const secondObservations: Array<Observation> = [{
+    fromChain: "erg",
+    toChain: "cardano",
+    fromAddress: "ergoAddress",
+    toAddress: "cardanoAddress",
+    amount: "1100000000",
+    fee: "1100000",
+    sourceChainTokenId: "ergoTokenId",
+    targetChainTokenId: "cardanoTokenId",
+    sourceTxId: "ergoTxId2",
+    sourceBlockId: "ergoBlockId",
+    requestId: "reqId2",
+}];
+
+
+describe("Database functions", async () => {
+    const DB = await loadDataBase("dataBase");
+
+    describe("saveBlock", () => {
+        it("should return true", async () => {
+            await DB.removeForkedBlocks(3433333);
+            let res = await DB.saveBlock(
+                3433333,
+                "26197be6579e09c7edec903239866fbe7ff6aee2e4ed4031c64d242e9dd1bff6",
+                "1111111111111111111111111111111111111111111111111111111111111111",
+                firstObservations
+            );
+            expect(res).to.be.true;
+            res = await DB.saveBlock(
+                3433334,
+                "19b60182cba99d621b3d02457fefb4cda81f4fbde3ca719617cbed2e4cc5c0ce",
+                "26197be6579e09c7edec903239866fbe7ff6aee2e4ed4031c64d242e9dd1bff6",
+                secondObservations
+            );
+            expect(res).to.be.true;
+        });
+    });
+
+    describe("getConfirmedObservations", () => {
+        it("returns 1 row", async () => {
+            const res = await DB.getConfirmedObservations(0);
+            expect(res).to.have.length(1)
+        });
+    });
+
+
+    describe("getLastSavedBlock", () => {
+        it("should return last saved block", async () => {
+            const lastBlock = await DB.getLastSavedBlock();
+            expect(lastBlock).to.eql({
+                "hash": "19b60182cba99d621b3d02457fefb4cda81f4fbde3ca719617cbed2e4cc5c0ce",
+                "parent_hash": "26197be6579e09c7edec903239866fbe7ff6aee2e4ed4031c64d242e9dd1bff6",
+                "block_height": 3433334
+            });
+        });
+    });
+
+    describe("getBlockAtHeight", () => {
+        it("should return block Hash", async () => {
+            const blockHash = await DB.getBlockAtHeight(3433333);
+            expect(blockHash?.hash).to.be.equal("26197be6579e09c7edec903239866fbe7ff6aee2e4ed4031c64d242e9dd1bff6");
+        });
+        it("should return block undefined", async () => {
+            const blockHash = await DB.getBlockAtHeight(3433222);
+            expect(blockHash).to.be.undefined;
+        });
+    });
+
+    describe("getCreatedCommitments", () => {
+        it("all stored bridge in the database has 'sent' flag, so it should return nothing", async () => {
+            const commitments = await DB.getCreatedCommitments()
+            expect(commitments.length).to.eql(0)
+        })
+        it("should return a created commitment", async () => {
+            // TODO: add a created commitment
+            // TODO: complete this test after updating the bridge
+            const commitments = await DB.getCreatedCommitments()
+            expect(commitments.length).to.eql(0)
+        })
+    })
+
+    describe("changeLastValidBlock", () => {
+        it("should affect 1 row", async () => {
+            const res = await DB.removeForkedBlocks(3433333);
+            expect(res.affected).to.be.equal(2);
+        });
+    });
+
+});
---
+++ cardano-scanner/tests/cardano/scanner/scanner.ts
@@ -0,0 +1,77 @@
+import { expect } from "chai";
+import { Scanner } from "../../../lib/scanner/scanner";
+import { firstObservations, loadDataBase } from "../models/models";
+import { KoiosNetwork } from "../../../lib/network/koios";
+import config from "config";
+
+describe("Scanner test", () => {
+    describe("isForkHappen", () => {
+        it("fork doesn't happened", async () => {
+            const DB = await loadDataBase("scanner");
+            await DB.removeForkedBlocks(3433333);
+            await DB.saveBlock(
+                3433333,
+                "26197be6579e09c7edec903239866fbe7ff6aee2e4ed4031c64d242e9dd1bff6",
+                "1111111111111111111111111111111111111111111111111111111111111111",
+                firstObservations
+            );
+            const koiosNetwork = new KoiosNetwork();
+            const scanner = new Scanner(DB, koiosNetwork, config);
+            expect(await scanner.isForkHappen()).to.equal(false);
+        });
+
+        it("fork happened", async () => {
+            const DB = await loadDataBase("scanner");
+            await DB.removeForkedBlocks(3433333);
+            await DB.saveBlock(
+                3433333,
+                "e1699582bd2e3426839e10f7f5066bafc6e3847fd4511a2013ba3b4e13514267",
+                "1111111111111111111111111111111111111111111111111111111111111111",
+                firstObservations
+            );
+            const koiosNetwork = new KoiosNetwork();
+            const scanner = new Scanner(DB, koiosNetwork, config);
+            expect(await scanner.isForkHappen()).to.be.true;
+        });
+
+        it("is undefined", async () => {
+            const DB = await loadDataBase("scanner-empty");
+            const koiosNetwork = new KoiosNetwork();
+            const scanner = new Scanner(DB, koiosNetwork, config);
+            expect(await scanner.isForkHappen()).to.be.false;
+        });
+
+    });
+    describe("update", () => {
+        it("scanner without fork", async () => {
+            const DB = await loadDataBase("scanner-without-fork");
+            await DB.removeForkedBlocks(3433333);
+            await DB.saveBlock(
+                3433333,
+                "26197be6579e09c7edec903239866fbe7ff6aee2e4ed4031c64d242e9dd1bff6",
+                "1111111111111111111111111111111111111111111111111111111111111111",
+                firstObservations
+            );
+            const koiosNetwork = new KoiosNetwork();
+            const scanner = new Scanner(DB, koiosNetwork, config);
+            await scanner.update(0);
+            const lastBlock = await DB.getLastSavedBlock();
+            expect(lastBlock?.block_height).to.be.equal(3433333);
+        });
+        // it("scanner without fork", async () => {
+        //     const DB = await loadDataBase("scanner-with-fork");
+        //     await DB.removeForkedBlocks(3433333);
+        //     await DB.saveBlock(
+        //         3433333,
+        //         "397e969e0525d82dc46a33e31634187dae94b12a6cc4b534e4e52f6d313aef22",
+        //         "1111111111111111111111111111111111111111111111111111111111111111",
+        //         firstObservations
+        //     );
+        //     const koiosNetwork = new KoiosNetwork();
+        //     const scanner = new Scanner(DB, koiosNetwork, config);
+        //     await scanner.update(0);
+        //     const lastBlock = await DB.getLastSavedBlock();
+        //     expect(lastBlock).to.be.undefined;
+        // });
+    })
+});
