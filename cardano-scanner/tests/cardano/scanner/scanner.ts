0fecc7263e6a928c688d5ea1238111aebdd138ca
sahandzou <sahandzoufan79@gmail.com>
2022-07-13 12:59:40 +0430

    cardano tests updated

--- cardano-scanner/tests/cardano/scanner/scanner.ts
+++ cardano-scanner/tests/cardano/scanner/scanner.ts
@@ -3,6 +3,20 @@ import { Scanner } from "../../../lib/scanner/scanner";
 import { firstObservations, loadDataBase } from "../models/models";
 import { KoiosNetwork } from "../../../lib/network/koios";
 
+const sampleObservation = {
+    fromChain: 'CARDANO',
+    toChain: 'ERGO',
+    fee: '10000',
+    amount: '10',
+    sourceChainTokenId: 'asset12y0ewmxggeglymjpmp9mjf5qzh4kgwj9chtkpv',
+    targetChainTokenId: 'cardanoTokenId',
+    sourceTxId: 'cf32ad374daefdce563e3391effc4fc42eb0e74bbec8afe16a46eeea69e3b2aa',
+    sourceBlockId: '93395496d590ec6db0f2fd13a7bcf91e82a9f230ef677f6216ea8c9f57df6ab3',
+    requestId: 'cf32ad374daefdce563e3391effc4fc42eb0e74bbec8afe16a46eeea69e3b2aa',
+    toAddress: 'ergoAddress',
+    fromAddress: 'cardanoAddress',
+};
+
 describe("Scanner test", () => {
     describe("isForkHappen", () => {
         it("fork doesn't happened", async () => {
@@ -72,5 +86,89 @@ describe("Scanner test", () => {
             const lastBlock = await DB.getLastSavedBlock();
             expect(lastBlock).to.be.undefined;
         });
+    })
+    describe("isRosenData", () => {
+        it("should be Rosen Data", async () => {
+            const DB = await loadDataBase("cardanoUtils");
+            const koiosNetwork = new KoiosNetwork();
+            const scanner = new Scanner(DB, koiosNetwork);
+            const validData = {
+                "to": "ERGO",
+                "fee": "10000",
+                "from": "CARDANO",
+                "toAddress": "ergoAddress",
+                "fromAddress": "cardanoAddress",
+                "targetChainTokenId": "cardanoTokenId",
+            };
+            expect(scanner.isRosenData(validData)).to.be.true;
+        });
+        it("should not be Rosen Data", async () => {
+            const DB = await loadDataBase("cardanoUtils");
+            const koiosNetwork = new KoiosNetwork();
+            const scanner = new Scanner(DB, koiosNetwork);
+            const invalidData = {
+                "to": "ERGO",
+                "fee": "10000",
+                "from": "CARDANO",
+            };
+            expect(scanner.isRosenData(invalidData)).to.be.false;
+        });
+    });
+    describe("isRosenMetaData", () => {
+        it("should be RosenMetaData", async () => {
+            const DB = await loadDataBase("cardanoUtils");
+            const koiosNetwork = new KoiosNetwork();
+            const scanner = new Scanner(DB, koiosNetwork);
+            const validMetaData = {
+                0: {}
+            }
+            expect(scanner.isRosenMetaData(validMetaData)).to.be.true;
+        });
+        it("should not be RosenMetaData", async () => {
+            const DB = await loadDataBase("cardanoUtils");
+            const koiosNetwork = new KoiosNetwork();
+            const scanner = new Scanner(DB, koiosNetwork);
+            const validMetaData = {
+                1: {}
+            }
+            expect(scanner.isRosenMetaData(validMetaData)).to.be.false;
+        });
+    });
+    describe("checkTx", () => {
+        // TODO: AssetFingerprint.fromParts
+        // it("should be observation", async () => {
+        //     const koiosNetwork = new KoiosNetwork();
+        //     const observation = await CardanoUtils.checkTx(
+        //         "cf32ad374daefdce563e3391effc4fc42eb0e74bbec8afe16a46eeea69e3b2aa"
+        //         , "93395496d590ec6db0f2fd13a7bcf91e82a9f230ef677f6216ea8c9f57df6ab3"
+        //         , ["addr_test1vze7yqqlg8cjlyhz7jzvsg0f3fhxpuu6m3llxrajfzqecggw704re"]
+        //         , koiosNetwork
+        //     );
+        //     expect(observation).to.be.eql(sampleObservation);
+        // });
+        it("should be undefined", async () => {
+            const DB = await loadDataBase("cardanoUtils");
+            const koiosNetwork = new KoiosNetwork();
+            const scanner = new Scanner(DB, koiosNetwork);
+            const observation = await scanner.checkTx(
+                "edce02f2f23ddc3270964d2ba74ff6375a5a78fd6caf1c66102565b83f5d3ca2"
+                , "93395496d590ec6db0f2fd13a7bcf91e82a9f230ef677f6216ea8c9f57df6ab3"
+                , ["addr_test1vze7yqqlg8cjlyhz7jzvsg0f3fhxpuu6m3llxrajfzqecggw704re"]
+                , koiosNetwork
+            );
+            expect(observation).to.be.undefined;
+        });
+    });
+    describe("observationAtHeight", () => {
+        const koiosNetwork = new KoiosNetwork();
+        // TODO: Should be fix AssetFingerprint.fromParts
+        // it("first index should be undefined and second be observation", async () => {
+        //     const observations = await CardanoUtils.observationsAtHeight(
+        //         "93395496d590ec6db0f2fd13a7bcf91e82a9f230ef677f6216ea8c9f57df6ab3"
+        //         , koiosNetwork
+        //     );
+        //     expect(observations.length).to.be.equal(1);
+        //     expect(observations[0]).to.be.eql(sampleObservation);
+        // });
     })
 });
