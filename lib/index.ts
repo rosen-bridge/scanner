15363a125f8677efba4aff84e62ad113dcd14830
sahandzou <sahandzoufan79@gmail.com>
2022-08-13 20:34:40 +0430

    exporting koiosTransaction

--- lib/index.ts
+++ lib/index.ts
@@ -3,4 +3,5 @@ export { CardanoKoiosScanner } from './scanner/cardano/cardano'
 export { migrations } from './migrations'
 export { BlockEntity } from './entities/blockEntity'
 export { AbstractExtractor } from './interfaces'
+export { KoiosTransaction } from './scanner/cardano/interfaces/Koios';
 export { AbstractScanner } from './scanner/abstract'
