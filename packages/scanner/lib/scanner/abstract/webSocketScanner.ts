6ca4358f1dab7173ba3db00cff6cd4c847ee940d
vorujack <vorujack@gmail.com>
2023-05-20 13:04:57 +0330

    update websocket and ogmios add websocket tests

--- packages/scanner/lib/scanner/abstract/webSocketScanner.ts
+++ packages/scanner/lib/scanner/abstract/webSocketScanner.ts
@@ -1,226 +1,89 @@
 import { AbstractScanner } from './scanner';
 import { Block } from '../../interfaces';
-import { Semaphore } from 'await-semaphore';
-import { AbstractLogger } from '@rosen-bridge/logger-interface';
-
-const MAX_PROCESS_TRANSACTION = 10;
-
-type QueueType<TransactionType> = {
-  block: Block;
-  transactions: Array<TransactionType>;
-  fork: boolean;
-  retriesCount?: number;
-};
+import { Mutex } from 'await-semaphore';
 
 abstract class WebSocketScanner<
   TransactionType
 > extends AbstractScanner<TransactionType> {
-  private semaphore = new Semaphore(1);
-  private processQueueSemaphore = new Semaphore(1);
-  private queue: Array<QueueType<TransactionType>> = [];
-  private stopLock = new Semaphore(1);
-  private stopFlag = false;
+  readonly MAX_PROCESS_TRANSACTION = 10;
+
+  private mutex = new Mutex();
   abstract name: () => string;
 
   abstract start: () => Promise<void>;
   abstract stop: () => Promise<void>;
 
-  constructor(
-    logger?: AbstractLogger,
-    public stopLimit = 100,
-    public restartPoint = 10
-  ) {
-    super(logger);
-  }
-
-  /**
-   * Insert new block to processing queue
-   * @param newBlock
-   * @param transactions
-   */
-  enqueueNewBlock = (newBlock: Block, transactions: Array<TransactionType>) => {
-    if (this.stopFlag) {
-      // Don't enqueue new block if scanner is stopped
-      return;
+  tryRunningFunction = async (
+    fn: () => Promise<boolean>,
+    msg: string
+  ): Promise<boolean> => {
+    for (
+      let tryRound = 1;
+      tryRound <= this.MAX_PROCESS_TRANSACTION;
+      tryRound++
+    ) {
+      if (await fn()) return true;
     }
-    this.semaphore.acquire().then((release) => {
-      const newQueueElement = { block: newBlock, transactions, fork: false };
-      let newElementIndex = 0;
-      while (
-        newElementIndex < this.queue.length &&
-        this.queue[newElementIndex].block.blockHeight < newBlock.blockHeight
-      ) {
-        newElementIndex++;
-      }
-      if (
-        this.queue.length > newElementIndex &&
-        this.queue[newElementIndex].block.blockHeight ===
-          newQueueElement.block.blockHeight
-      ) {
-        this.queue[newElementIndex] = newQueueElement;
-      } else {
-        this.queue = [
-          ...this.queue.slice(0, newElementIndex),
-          newQueueElement,
-          ...this.queue.slice(newElementIndex),
-        ];
-      }
-      release();
-    });
-  };
-
-  /**
-   * Enqueue new fork to processing queue.
-   * First remove forked blocks from the queue and then insert fork to the queue.
-   * @param lastValidBlock
-   */
-  enqueueNewFork = (lastValidBlock: Block) => {
-    this.semaphore.acquire().then((release) => {
-      const newQueueElement = {
-        block: lastValidBlock,
-        transactions: [],
-        fork: false,
-      };
-      let forkedBlockIndex = 0;
-      while (
-        forkedBlockIndex < this.queue.length &&
-        this.queue[forkedBlockIndex].block.blockHeight <
-          lastValidBlock.blockHeight
-      ) {
-        forkedBlockIndex++;
-      }
-      this.queue = [...this.queue.slice(0, forkedBlockIndex), newQueueElement];
-      release();
-    });
-  };
-
-  /**
-   * Process single Queued element.
-   * In case of a fork, remove all next blocks and their corresponding extracted content.
-   * In case of a new block, store block and extract information from transactions.
-   * If any exception happen, rethrow it.
-   * @param element
-   */
-  processElement = async (element: QueueType<TransactionType>) => {
-    if (element.fork) {
-      this.logger.debug(
-        `Processing forked block at height ${element.block.blockHeight}`
-      );
-      return await this.forkBlock(element.block.blockHeight);
-    } else {
-      this.logger.debug(
-        `Processing new block at height ${element.block.blockHeight}`
-      );
-      await this.forkBlock(element.block.blockHeight);
-      const lastSavedBlock = await this.action.getLastSavedBlock();
-      if (lastSavedBlock && element.block.parentHash !== lastSavedBlock.hash) {
-        throw Error('It seems saved block is not valid in scanner.');
-      }
-      const res = await this.processBlockTransactions(
-        element.block,
-        element.transactions
-      );
-      if (res === false) {
-        throw Error(
-          `Can not process block at height ${element.block.blockHeight}`
-        );
-      }
-    }
-  };
-
-  /**
-   * Process block queue elements.
-   */
-  processQueue = async () => {
-    const releaseProcessQueue = await this.processQueueSemaphore.acquire();
-    if (this.queue.length > 0) {
-      const queuedElement = this.queue[0];
-      try {
-        await this.processElement(queuedElement);
-        const release = await this.semaphore.acquire();
-        if (this.queue[0] === queuedElement) {
-          this.queue.splice(0, 1);
-        }
-        if (this.queue.length > 0) {
-          // try processing next block asynchronously
-          this.processQueue();
-        }
-        release();
-      } catch (e) {
-        const logMessage = `Can not process block at height ${queuedElement.block.blockHeight}: ${e}`;
-        if (queuedElement.retriesCount == MAX_PROCESS_TRANSACTION) {
-          this.logger.error(logMessage);
-        } else {
-          this.logger.warn(logMessage);
-          queuedElement.retriesCount = queuedElement.retriesCount
-            ? queuedElement.retriesCount + 1
-            : 1;
-          this.logger.warn(
-            `retrying storing this block in 100 ms... try ${queuedElement.retriesCount}`
-          );
-          // try processing queue element again
-          this.processQueue();
-        }
-      }
-    }
-    await this.checkQueueRestartPoint();
-    releaseProcessQueue();
+    this.logger.error(
+      `${msg} can not be proceed in ${this.MAX_PROCESS_TRANSACTION} try. scanner restarted automatically`
+    );
+    await this.stop();
+    await this.start();
+    return false;
   };
 
   /**
    * Handle new block arrived.
+   * save block and its transactions.
    * @param block
    * @param transactions
    */
-  forwardBlock = async (block: Block, transactions: Array<TransactionType>) => {
-    await this.enqueueNewBlock(block, transactions);
-    // Running transaction queue asynchronously
-    this.processQueue();
-    // Handle memory usage
-    await this.checkQueueStopLimit();
+  stepForward = async (block: Block, transactions: Array<TransactionType>) => {
+    const release = await this.mutex.acquire();
+    await this.tryRunningFunction(async () => {
+      try {
+        await this.forkBlock(block.blockHeight);
+
+        const lastSavedBlock = await this.action.getLastSavedBlock();
+        if (lastSavedBlock && block.parentHash !== lastSavedBlock.hash) {
+          this.logger.error('It seems saved block is not valid in scanner.');
+          return false;
+        } else {
+          const res = await this.processBlockTransactions(block, transactions);
+          if (res === false) {
+            this.logger.error(
+              `Can not process block at height ${block.blockHeight}`
+            );
+          } else {
+            return true;
+          }
+        }
+      } catch (e: any) {
+        this.logger.warn(`unknown error occurred ${e}`);
+        this.logger.warn(e.stack);
+      }
+      return false;
+    }, `Block at height ${block.blockHeight}`);
+    release();
   };
 
   /**
    * Handle forking a block
    * @param block
    */
-  backwardBlock = async (block: Block) => {
-    await this.enqueueNewFork(block);
-    this.processQueue();
-  };
-
-  /**
-   * Check the queue, if reached stop limit, stop scanner.
-   */
-  checkQueueStopLimit = async () => {
-    if (!this.stopFlag && this.queue.length > this.stopLimit) {
-      const releaseStopLock = await this.stopLock.acquire();
-      this.logger.warn(
-        `Queue length is more than ${
-          this.stopLimit
-        }. Stopping ${this.name()} scanner...`
-      );
-      this.stopFlag = true;
-      await this.stop();
-      releaseStopLock();
-    }
-  };
-
-  /**
-   * Check the queue, if reached restart point, start scanner.
-   */
-  checkQueueRestartPoint = async () => {
-    if (this.stopFlag && this.queue.length <= this.restartPoint) {
-      const releaseStopLock = await this.stopLock.acquire();
-      this.logger.warn(
-        `Queue length is less than ${
-          this.restartPoint
-        }. Restarting ${this.name()} scanner...`
-      );
-      this.stopFlag = false;
-      await this.start();
-      releaseStopLock();
-    }
+  stepBackward = async (block: Block) => {
+    const release = await this.mutex.acquire();
+    await this.tryRunningFunction(async () => {
+      try {
+        await this.forkBlock(block.blockHeight);
+        return true;
+      } catch (e: any) {
+        this.logger.error(`unknown error occurred ${e}`);
+        this.logger.error(e.stack);
+      }
+      return false;
+    }, `Forking block at height ${block.blockHeight}`);
+    release();
   };
 }
 
---
+++ packages/scanner/tests/scanner/abstract/webSocketScanner.spec.ts
@@ -0,0 +1,157 @@
+import {
+  createDatabase,
+  FailExtractor,
+  TestWebSocketScanner,
+} from './abstract.mock';
+import { DataSource, Repository } from 'typeorm';
+import { BlockEntity, PROCEED } from '../../../lib';
+import { DummyLogger } from '@rosen-bridge/logger-interface';
+
+let dataSource: DataSource;
+let scanner: TestWebSocketScanner;
+let repository: Repository<BlockEntity>;
+
+describe('webSocketScanner', () => {
+  beforeEach(async () => {
+    dataSource = await createDatabase();
+    scanner = new TestWebSocketScanner(new DummyLogger(), dataSource);
+    repository = dataSource.getRepository(BlockEntity);
+    await repository.insert({
+      hash: 'block 1',
+      parentHash: 'block 0',
+      height: 100,
+      status: PROCEED,
+      scanner: scanner.name(),
+    });
+  });
+
+  describe('tryRunningFunction', () => {
+    /**
+     * @target webSocketScanner.tryRunningFunction should call fn once and return true
+     * @dependency
+     * @scenario
+     * - call tryRunningFunction with call back function which not throw exception
+     * @expected
+     * - must called callback once
+     * - must return true
+     */
+    it('should call fn once and return true ', async () => {
+      let callCount = 0;
+      const mockFn = async () => {
+        callCount += 1;
+        return true;
+      };
+      const res = await scanner.tryRunningFunction(mockFn, '');
+      expect(res).toBeTruthy();
+      expect(callCount).toEqual(1);
+    });
+    /**
+     * @target webSocketScanner.tryRunningFunction should call fn once and return true
+     * @dependency
+     * @scenario
+     * - call tryRunningFunction with call back function which not throw exception
+     * @expected
+     * - must call callback 10 times
+     * - must return false
+     */
+    it('should called fn 10 time if returns false and return false ', async () => {
+      let callCount = 0;
+      const mockFn = async () => {
+        callCount += 1;
+        return false;
+      };
+      const res = await scanner.tryRunningFunction(mockFn, '');
+      expect(res).toBeFalsy();
+      expect(callCount).toEqual(scanner.MAX_PROCESS_TRANSACTION);
+    });
+  });
+
+  describe('stepForward', () => {
+    /**
+     * @target webSocketScanner.stepForward should not insert block in database
+     * if current block hash not equals last inserted one
+     * @dependency
+     * @scenario
+     * - insert a block into database
+     * - call stepForward with a block with different parent hash of inserted block
+     * @expected
+     * - no block inserted to database
+     */
+    it('should not insert block in database if current block hash not equals last inserted one', async () => {
+      await scanner.stepForward(
+        { hash: 'block 2', parentHash: 'block 0', blockHeight: 101, extra: '' },
+        []
+      );
+      expect((await repository.findBy({ height: 101 })).length).toEqual(0);
+    });
+
+    /**
+     * @target webSocketScanner.stepForward should not insert any block into database if process extractor fails to process transactions
+     * @dependency
+     * @scenario
+     * - insert a block into database
+     * - register fail extractor
+     * - call stepForward with a block with a block and no transaction
+     * @expected
+     * - no block inserted to database
+     */
+    it('should return false if block parent hash is not equals to current block hash', async () => {
+      scanner.registerExtractor(new FailExtractor());
+      await scanner.stepForward(
+        { hash: 'block 2', parentHash: 'block 0', blockHeight: 101, extra: '' },
+        []
+      );
+      expect((await repository.findBy({ height: 101 })).length).toEqual(0);
+    });
+    /**
+     * @target webSocketScanner.stepForward should store block into database
+     * @dependency
+     * @scenario
+     * - insert a block into database
+     * - call stepForward with a block
+     * @expected
+     * - must store a block entity to database with correct values
+     */
+    it('should return false if block parent hash is not equals to current block hash', async () => {
+      await scanner.stepForward(
+        { hash: 'block 2', parentHash: 'block 1', blockHeight: 101, extra: '' },
+        []
+      );
+      const inserted = await repository.findBy({ height: 101 });
+      expect(inserted.length).toBe(1);
+      const block = inserted[0];
+      expect(block.hash).toEqual('block 2');
+      expect(block.parentHash).toEqual('block 1');
+      expect(block.scanner).toEqual(scanner.name());
+    });
+  });
+
+  describe('stepBackward', () => {
+    /**
+     * @target webSocketScanner.stepBackward should delete block from database
+     * @dependency
+     * @scenario
+     * - insert two block into database
+     * - call stepBackward
+     * @expected
+     * - the latest block removed from database
+     */
+    it('should delete block from database', async () => {
+      await repository.insert({
+        hash: 'block 2',
+        parentHash: 'block 1',
+        height: 101,
+        status: PROCEED,
+        scanner: scanner.name(),
+      });
+      await scanner.stepBackward({
+        blockHeight: 100,
+        hash: 'block 1',
+        parentHash: 'block 0',
+        extra: '',
+      });
+      const elements = await repository.findBy({ height: 101 });
+      expect(elements.length).toEqual(0);
+    });
+  });
+});
