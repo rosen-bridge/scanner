67bbf6f76a5abbb31039a30bf3da26dbbd1cfa76
HFazelinia <hadifazelinia78@gmail.com>
2023-10-13 14:23:28 +0100

    update Cardano native token name to 'ada'

--- packages/observation-extractor/lib/extractor/const.ts
+++ packages/observation-extractor/lib/extractor/const.ts
@@ -1,3 +1,3 @@
-export const CARDANO_NATIVE_TOKEN = 'lovelace';
+export const CARDANO_NATIVE_TOKEN = 'ada';
 export const ERGO_NATIVE_TOKEN = 'erg';
 export const NUMBER_OF_BLOCKS_PER_YEAR = 30 * 24 * 365;
