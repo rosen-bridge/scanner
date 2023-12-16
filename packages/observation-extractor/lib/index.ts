6700c0bfcf49ae2c0a70eb599d4c0f7a9eeccb9a
HFazelinia <hadifazelinia78@gmail.com>
2023-12-16 21:18:56 +0000

    export BlockFrost scanner and extractor

--- packages/observation-extractor/lib/index.ts
+++ packages/observation-extractor/lib/index.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 export { CardanoKoiosObservationExtractor } from './extractor/cardano/koios';
 export { CardanoOgmiosObservationExtractor } from './extractor/cardano/ogmios';
+export { CardanoBlockFrostObservationExtractor } from './extractor/cardano/blockfrost';
 export { ErgoObservationExtractor } from './extractor/ergo/ergoExtractor';
 export { ObservationEntity } from './entities/observationEntity';
 export { migrations } from './migrations/index';
