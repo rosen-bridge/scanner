d5c50613a56d16f161815eb3b0b1965206414faf
HFazelinia <hadifazelinia78@gmail.com>
2023-12-18 12:16:27 +0000

    add tests for CardanoGraphQLObservationExtractor

--- packages/observation-extractor/lib/index.ts
+++ packages/observation-extractor/lib/index.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata';
 export { CardanoKoiosObservationExtractor } from './extractor/cardano/koios';
 export { CardanoOgmiosObservationExtractor } from './extractor/cardano/ogmios';
 export { CardanoBlockFrostObservationExtractor } from './extractor/cardano/blockfrost';
+export { CardanoGraphQLObservationExtractor } from './extractor/cardano/graphql';
 export { ErgoObservationExtractor } from './extractor/ergo/ergoExtractor';
 export { ObservationEntity } from './entities/observationEntity';
 export { migrations } from './migrations/index';
