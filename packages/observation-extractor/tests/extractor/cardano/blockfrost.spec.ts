e56d6c90392ba8def9d3d2df62936c512e54fedb
HFazelinia <hadifazelinia78@gmail.com>
2023-12-18 07:58:52 +0000

    add unit tests

---
+++ packages/observation-extractor/tests/extractor/cardano/blockfrost.spec.ts
@@ -0,0 +1,121 @@
+import { CardanoBlockFrostObservationExtractor } from '../../../lib';
+import { generateBlockEntity, createDatabase } from '../utils.mock';
+import { ObservationEntity } from '../../../lib';
+import { tokens } from '../tokens.mock';
+import { Buffer } from 'buffer';
+import { blake2b } from 'blakejs';
+import { ERGO_NATIVE_TOKEN } from '../../../lib/extractor/const';
+import { DataSource } from 'typeorm';
+import { validLockTx } from './blockFrostTestData';
+
+class CardanoBlockFrostExtractor extends CardanoBlockFrostObservationExtractor {}
+
+const bankAddress =
+  'addr_test1vze7yqqlg8cjlyhz7jzvsg0f3fhxpuu6m3llxrajfzqecggw704re';
+
+let dataSource: DataSource;
+
+describe('cardanoBlockFrostObservationExtractor', () => {
+  beforeEach(async () => {
+    dataSource = await createDatabase();
+  });
+  describe('processTransactionsCardano', () => {
+    /**
+     * @target cardanoBlockFrostObservationExtractor.processTransactionsCardano
+     * should return true and insert observation into database on valid lock tx
+     * @dependencies
+     * - cardanoKoiosClientFactory
+     * @scenario
+     * - mock lock tx
+     * - run test
+     * - check returned value
+     * - check database
+     * @expected
+     * - it should return true
+     * - observation should be inserted into database
+     */
+    it('should return true and insert observation into database on valid lock tx', async () => {
+      // mock lock tx
+      const tx = validLockTx;
+
+      // run test
+      const extractor = new CardanoBlockFrostExtractor(
+        dataSource,
+        tokens,
+        bankAddress
+      );
+      const res = await extractor.processTransactions(
+        [tx],
+        generateBlockEntity(dataSource, '1')
+      );
+
+      // check returned valid
+      expect(res).toEqual(true);
+
+      // check database
+      const repository = dataSource.getRepository(ObservationEntity);
+      const [rows, rowsCount] = await repository.findAndCount();
+      expect(rowsCount).toEqual(1);
+      const observation1 = rows[0];
+      const txHash = validLockTx.utxos.hash;
+      expect(observation1).toEqual({
+        id: 1,
+        fromChain: 'cardano',
+        toChain: 'ergo',
+        fromAddress:
+          'addr_test1vzg07d2qp3xje0w77f982zkhqey50gjxrsdqh89yx8r7nasu97hr0',
+        toAddress: 'ergoAddress',
+        height: 1,
+        amount: '3000000000',
+        networkFee: '9842520',
+        bridgeFee: '1968503938',
+        sourceChainTokenId:
+          'ace7bcc2ce705679149746620de3a84660ce57573df54b5a096e39a2.7369676d61',
+        targetChainTokenId: ERGO_NATIVE_TOKEN,
+        sourceBlockId: '1',
+        sourceTxId: txHash,
+        block: '1',
+        requestId: Buffer.from(blake2b(txHash, undefined, 32)).toString('hex'),
+        extractor: 'ergo-cardano-blockfrost-extractor',
+      });
+    }, 100000);
+
+    /**
+     * @target cardanoBlockFrostObservationExtractor.processTransactionsCardano
+     * should return true with no observation in database on invalid lock tx
+     * @dependencies
+     * - cardanoKoiosClientFactory
+     * @scenario
+     * - mock lock tx
+     * - run test
+     * - check returned value
+     * - check database
+     * @expected
+     * - it should return true
+     * - no observation should be in database
+     */
+    it('should return true with no observation in database on invalid lock tx', async () => {
+      // mock lock tx
+      const tx = validLockTx;
+
+      // run test
+      const extractor = new CardanoBlockFrostExtractor(
+        dataSource,
+        tokens,
+        'addr_test1qq5qeusgymq8ledv9gltp9fuh5jchetjeafha75n6dghur4gtzcgx'
+      );
+      const res = await extractor.processTransactions(
+        [tx],
+        generateBlockEntity(dataSource, '1')
+      );
+
+      // check returned valid
+      expect(res).toEqual(true);
+
+      // check database
+      const repository = dataSource.getRepository(ObservationEntity);
+      const [rows, rowsCount] = await repository.findAndCount();
+      expect(rowsCount).toEqual(0);
+    }, 100000);
+  });
+});
