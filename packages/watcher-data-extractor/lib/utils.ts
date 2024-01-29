71208bb65a695346e10fa58b585a8b76f25a8c22
HFazelinia <hadifazelinia78@gmail.com>
2024-01-29 04:14:45 +0000

    fix bug in WIDsHash calculation

--- packages/watcher-data-extractor/lib/utils.ts
+++ packages/watcher-data-extractor/lib/utils.ts
@@ -10,12 +10,15 @@ const JsonBI = JSONBigInt({
  * @param WIDs
  * @returns returns WIDsHash with it's count
  */
-const getWidInfo = (WIDs: string[]) => {
-  const hash = Buffer.from(blake2b(WIDs.join(), undefined, 32)).toString('hex');
+const getWidInfo = (WIDs: string) => {
+  const WIDsArray = WIDs.split(',');
+  const hash = Buffer.from(blake2b(WIDsArray.join(''), undefined, 32)).toString(
+    'hex'
+  );
 
   return {
     WIDsHash: hash,
-    WIDsCount: WIDs.length,
+    WIDsCount: WIDsArray.length,
   };
 };
 
