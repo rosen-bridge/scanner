708d40211c4f5d7572dfe25cdfe1d7d9e57a6ca7
HFazelinia <hadifazelinia78@gmail.com>
2024-04-21 11:23:01 +0100

    export migrations

--- packages/evm-address-tx-extractor/lib/index.ts
+++ packages/evm-address-tx-extractor/lib/index.ts
@@ -2,3 +2,4 @@ export * from './interfaces/types';
 export * from './extractors/EvmTxExtractor';
 export * from './entities/AddressTxsEntity';
 export * from './actions/db';
+export * from './migrations';
