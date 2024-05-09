f58fb76cfc620d55e0131fe5f32599530c220599
fateme.r <fatemeh.r3366@gmail.com>
2024-05-09 17:37:58 +0330

    Fix unit tests

--- packages/extractor/lib/constants.ts
+++ packages/extractor/lib/constants.ts
@@ -1,1 +1,2 @@
 export const API_LIMIT = 100;
+export const RETRIAL_COUNT = 10;
--- packages/extractor/lib/ergo/initializable/abstract.ts
+++ packages/extractor/lib/ergo/initializable/abstract.ts
@@ -3,7 +3,7 @@ import { AbstractLogger } from '@rosen-bridge/abstract-logger';
 import JsonBigInt from '@rosen-bridge/json-bigint';
 
 import { OutputBox, ErgoBox, ErgoExtractedData } from '../interfaces';
-import { API_LIMIT } from '../../constants';
+import { API_LIMIT, RETRIAL_COUNT } from '../../constants';
 import { AbstractErgoExtractor } from '../abstract';
 import { AbstractInitializableErgoExtractorAction } from './abstractAction';
 import { BlockInfo } from '../../interfaces';
@@ -90,6 +90,7 @@ export abstract class AbstractInitializableErgoExtractor<
    * Initializes the database with older boxes related to the address
    */
   initializeBoxes = async (initialBlock: BlockInfo) => {
+    let trial = 1;
     if (this.initialize) {
       this.logger.debug(
         `Initializing ${this.getId()} started, removing all existing data`
@@ -99,20 +100,34 @@ export abstract class AbstractInitializableErgoExtractor<
       let hasNextBatch = true;
       let offset = 0;
       while (hasNextBatch) {
-        const data = await this.fetchDataWithOffsetLimit(
-          initialBlock.height,
-          offset,
-          API_LIMIT
-        );
-        this.logger.info(
-          `Inserting ${
-            data.extractedBoxes.length
-          } new extracted data in ${this.getId()} initialization`
-        );
-        await this.actions.insertBoxes(data.extractedBoxes, this.getId());
+        try {
+          const data = await this.fetchDataWithOffsetLimit(
+            initialBlock.height,
+            offset,
+            API_LIMIT
+          );
+          this.logger.info(
+            `Inserting ${
+              data.extractedBoxes.length
+            } new extracted data in ${this.getId()} initialization`
+          );
+          await this.actions.insertBoxes(data.extractedBoxes, this.getId());
 
-        hasNextBatch = data.hasNextBatch;
-        offset += API_LIMIT;
+          hasNextBatch = data.hasNextBatch;
+          offset += API_LIMIT;
+        } catch (e) {
+          this.logger.warn(
+            `Initialization for ${this.getId()} failed with error :${e}`
+          );
+          if (trial == RETRIAL_COUNT)
+            throw Error(
+              `Initialization for ${this.getId()} failed after ${RETRIAL_COUNT} retrial`
+            );
+          trial += 1;
+          this.logger.debug(
+            `Trying again to initialize ${this.getId()} with trial step ${trial}`
+          );
+        }
       }
     } else {
       this.logger.info(`Initialization for ${this.getId()} is turned off`);
