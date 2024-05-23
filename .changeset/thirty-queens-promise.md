b0d28089b322e700a551a7530cfa2cb5f3ef3e3b
fateme.r <fatemeh.r3366@gmail.com>
2024-05-23 12:04:03 +0330

    Export constants and abstract network interface

---
+++ .changeset/thirty-queens-promise.md
@@ -0,0 +1,5 @@
+---
+'@rosen-bridge/abstract-extractor': patch
+---
+
+Export required interfaces
--- packages/abstract-extractor/lib/ergo/index.ts
+++ packages/abstract-extractor/lib/ergo/index.ts
@@ -3,5 +3,6 @@ export * from './AbstractErgoExtractorAction';
 export * from './interfaces';
 export * from './network/ExplorerNetwork';
 export * from './network/NodeNetwork';
+export * from './network/AbstractNetwork';
 export * from './initializable';
 export * from './utils';
