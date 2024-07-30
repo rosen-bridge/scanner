08d1d81f2ac4ade2a001536de8bbd631017217ec
HFazelinia <hadifazelinia78@gmail.com>
2024-07-30 16:54:00 +0100

    fix postgres migrations

--- packages/evm-address-tx-extractor/lib/migrations/postgres/1722346608595-migration.ts
+++ packages/evm-address-tx-extractor/lib/migrations/postgres/1722346608595-migration.ts
@@ -5,35 +5,14 @@ export class Migration1722346608595 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            CREATE TABLE "extractor_status_entity" (
-                "scannerId" character varying NOT NULL,
-                "extractorId" character varying NOT NULL,
-                "updateHeight" integer NOT NULL,
-                "updateBlockHash" character varying NOT NULL,
-                CONSTRAINT "PK_74b8d00f8f0bbfc3814ef77e07e" PRIMARY KEY ("scannerId", "extractorId")
-            )
-        `);
-    await queryRunner.query(`
-            CREATE TABLE "address_txs_entity" (
-                "id" SERIAL NOT NULL,
-                "unsignedHash" character varying NOT NULL,
-                "signedHash" character varying NOT NULL,
-                "nonce" integer NOT NULL,
-                "address" character varying NOT NULL,
-                "blockId" character varying NOT NULL,
-                "extractor" character varying NOT NULL,
-                "status" character varying NOT NULL,
-                CONSTRAINT "PK_3d12ab238cb8eed0354cec04ea4" PRIMARY KEY ("id")
-            )
-        `);
+          ALTER TABLE "address_txs_entity"
+          ADD "status" character varying NOT NULL
+      `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            DROP TABLE "address_txs_entity"
-        `);
-    await queryRunner.query(`
-            DROP TABLE "extractor_status_entity"
-        `);
+          ALTER TABLE "address_txs_entity" DROP COLUMN "status"
+      `);
   }
 }
