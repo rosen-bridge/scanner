e500d98351350982043af822b5ae32292ab2d801
fateme.r <fatemeh.r3366@gmail.com>
2024-12-28 17:23:47 +0330

    Implement parallel range search using multiple threads
    - Divide the height range: Split the height range into segments,
    assigning each segment to a separate thread to perform parallel searches
    and store relevant information.
    - Leverage range list data structure: Each thread should maintain its
    own range list data structure, allowing it to use previously retrieved
    data to reduce the number of queries significantly.
    - Dynamic range allocation: Once a thread completes its current job, it
    dynamically splits the largest remaining range between itself and an
    actively working thread to maximize efficiency.

--- packages/abstract-extractor/lib/ergo/initializable/ExplorerInitializer.ts
+++ packages/abstract-extractor/lib/ergo/initializable/ExplorerInitializer.ts
@@ -3,26 +3,27 @@ import {
   ErgoExtractedData,
   ExtendedTransaction,
   RangeList,
-  RangeQuery,
 } from '../interfaces';
 import { BlockInfo } from '../../interfaces';
 import { ExplorerNetwork } from '../network/ExplorerNetwork';
 import { API_LIMIT } from '../../constants';
-import { requestWithRetrial } from '../utils';
+import { delay, requestWithRetrial } from '../utils';
 import { DummyLogger } from '@rosen-bridge/abstract-logger';
 import PQueue from 'p-queue';
+import { Mutex } from 'await-semaphore';
 
 export class ExplorerInitializer<ExtractedData extends ErgoExtractedData> {
   private network: ExplorerNetwork;
-  private rangeList: RangeList;
+  private rangeLists: RangeList[];
   private extraLargeBlocks: BlockInfo[];
   private promiseQueue: PQueue;
+  private mutex = new Mutex();
 
   constructor(
     private extractor: AbstractInitializableErgoExtractor<ExtractedData>,
     url: string,
     private address: string,
-    maxParallelRequests: number,
+    private maxParallelRequests: number,
     private processTransactionBatch: (
       txs: ExtendedTransaction[]
     ) => Promise<void>,
@@ -49,7 +50,10 @@ export class ExplorerInitializer<ExtractedData extends ErgoExtractedData> {
   };
 
   processRange = async (start: number, end: number, count?: number) => {
-    if (count && count == 0) return 0;
+    if (count != undefined && count == 0) {
+      this.logger.debug(`skipping range [${start}, ${end}] with 0 txs`);
+      return 0;
+    }
     const txs = await requestWithRetrial(
       () =>
         this.network.getAddressTransactionsWithHeight(this.address, start, end),
@@ -61,11 +65,11 @@ export class ExplorerInitializer<ExtractedData extends ErgoExtractedData> {
       );
     if (txs.total <= API_LIMIT && txs.total > 0) {
       this.logger.debug(
-        `Processing started for [${start}, ${end}] with ${txs.total} txs}`
+        `Processing started for [${start}, ${end}] with ${txs.total} txs`
       );
       await this.processTransactionBatch(txs.items);
       this.logger.debug(
-        `Processing finished for [${start}, ${end}] with ${txs.total} txs}`
+        `Processing finished for [${start}, ${end}] with ${txs.total} txs`
       );
     }
     return txs.total;
@@ -91,19 +95,33 @@ export class ExplorerInitializer<ExtractedData extends ErgoExtractedData> {
     this.extraLargeBlocks.push(block);
   };
 
-  updateRangeList = (start: number, processedTxs: number) => {
-    this.rangeList.map((rangeQuery) => {
+  updateRangeList = (
+    rangeIndex: number,
+    start: number,
+    processedTxs: number
+  ) => {
+    this.rangeLists[rangeIndex].map((rangeQuery) => {
       rangeQuery.count -= processedTxs;
       rangeQuery.start = start;
     });
   };
 
-  searchRange = async () => {
-    while (this.rangeList.length > 0) {
+  searchRange = async (rangeIndex: number) => {
+    const rangeList = this.rangeLists[rangeIndex];
+    while (rangeList.length > 0) {
       this.logger.debug(
-        `Search range list is ${JSON.stringify(this.rangeList)}`
+        `Search range list ${rangeIndex} is ${JSON.stringify(rangeList)}`
       );
-      const lastRangeQuery = this.rangeList.at(-1)!;
+      let lastRangeQuery = rangeList.at(-1)!;
+      const release = await this.mutex.acquire();
+      while (
+        rangeList.length > 1 &&
+        (rangeList.at(-2)!.count <= API_LIMIT || lastRangeQuery.count == 0)
+      ) {
+        rangeList.pop()!;
+        lastRangeQuery = rangeList.at(-1)!;
+      }
+      release();
       this.logger.debug(
         `Checking range query ${JSON.stringify(lastRangeQuery)}`
       );
@@ -129,12 +147,16 @@ export class ExplorerInitializer<ExtractedData extends ErgoExtractedData> {
               newRangeQuery
             )}`
           );
-          this.rangeList.push(newRangeQuery);
+          const release = await this.mutex.acquire();
+          rangeList.push(newRangeQuery);
+          release();
         } else {
           this.logger.debug(
             `Processed range [${lastRangeQuery.start}, ${newQueryEnd}] with ${newQueryCount} txs in first round`
           );
-          this.updateRangeList(newQueryEnd + 1, newQueryCount);
+          const release = await this.mutex.acquire();
+          this.updateRangeList(rangeIndex, newQueryEnd + 1, newQueryCount);
+          release();
         }
       } else {
         if (lastRangeQuery.count <= API_LIMIT) {
@@ -150,10 +172,71 @@ export class ExplorerInitializer<ExtractedData extends ErgoExtractedData> {
           );
           await this.processBlockAtHeight(lastRangeQuery.start);
         }
-        this.rangeList.pop();
-        this.updateRangeList(lastRangeQuery.end + 1, lastRangeQuery.count);
+        const release = await this.mutex.acquire();
+        rangeList.pop();
+        this.updateRangeList(
+          rangeIndex,
+          lastRangeQuery.end + 1,
+          lastRangeQuery.count
+        );
+        release();
       }
     }
+  };
+
+  getBiggestIncompleteRangeIndex = (): number | undefined => {
+    let biggestRange = 0,
+      biggestRangeIndex = undefined;
+    for (let i = 0; i < this.rangeLists.length; i++) {
+      const rangeList = this.rangeLists[i];
+      if (rangeList.length > 1 && rangeList[0].count > biggestRange) {
+        biggestRange = rangeList[0].count;
+        biggestRangeIndex = i;
+      }
+    }
+    return biggestRangeIndex;
+  };
+
+  splitRanges = async (): Promise<number | undefined> => {
+    const release = await this.mutex.acquire();
+    this.logger.debug(
+      `Range lists before splitting ${JSON.stringify(this.rangeLists)}`
+    );
+    const biggestIncompleteRangeIndex = this.getBiggestIncompleteRangeIndex();
+    if (biggestIncompleteRangeIndex == undefined) {
+      this.logger.debug(`there is no incomplete range to reassign`);
+      release();
+      return;
+    }
+    this.logger.debug(
+      `biggest incomplete range is ${JSON.stringify(
+        this.rangeLists[biggestIncompleteRangeIndex]
+      )}`
+    );
+    const firstEmptySlot = this.rangeLists.findIndex(
+      (rangeList) => rangeList.length == 0
+    );
+    if (firstEmptySlot == -1) {
+      this.logger.debug(`There is no empty slot to reassign`);
+      release();
+      return;
+    }
+    this.logger.debug(`first empty slot is ${firstEmptySlot}`);
+    const removedRangeQuery =
+      this.rangeLists[biggestIncompleteRangeIndex].shift()!;
+    const head = this.rangeLists[biggestIncompleteRangeIndex][0];
+    this.rangeLists[firstEmptySlot] = [
+      {
+        start: head.end + 1,
+        end: removedRangeQuery.end,
+        count: removedRangeQuery.count - head.count,
+      },
+    ];
+    release();
+    this.logger.debug(
+      `Range lists after splitting ${JSON.stringify(this.rangeLists)}`
+    );
+    return firstEmptySlot;
   };
 
   /**
@@ -161,17 +244,40 @@ export class ExplorerInitializer<ExtractedData extends ErgoExtractedData> {
    * @param initialBlock
    */
   initialize = async (initialBlock: BlockInfo) => {
-    this.rangeList = [
-      {
-        start: 0,
-        end: initialBlock.height,
-        count: await this.getRangeTxCount(0, initialBlock.height),
-      },
-    ];
-    await this.searchRange();
-    // for (const block of this.extraLargeBlocks) {
-    //   await this.processBlock(block);
-    // }
+    this.rangeLists = [];
+    const segmentSize = Math.ceil(
+      initialBlock.height / this.maxParallelRequests
+    );
+    const startSearch = (i: number) =>
+      this.promiseQueue.add(() => this.searchRange(i));
+    this.promiseQueue.on('completed', async () => {
+      this.logger.debug(
+        `########################################### ${JSON.stringify(
+          this.rangeLists
+        )}`
+      );
+      const newRangeIndex = await this.splitRanges();
+      this.logger.debug(
+        `&&&&&&&&&&&&&&&&&&&&&& reassigned range ${newRangeIndex}`
+      );
+      if (newRangeIndex != undefined) startSearch(newRangeIndex);
+    });
+    for (let i = this.maxParallelRequests; i > 0; i--) {
+      const start = (i - 1) * segmentSize;
+      const end = Math.min(i * segmentSize - 1, initialBlock.height);
+      this.rangeLists.push([
+        {
+          start: start,
+          end: end,
+          count: await this.getRangeTxCount(start, end),
+        },
+      ]);
+      this.logger.debug('&&&&&&&&&&&&& ' + JSON.stringify(this.rangeLists));
+      startSearch(this.maxParallelRequests - i);
+    }
     await this.promiseQueue.onIdle();
+    for (const block of this.extraLargeBlocks) {
+      await this.processBlock(block);
+    }
   };
 }
