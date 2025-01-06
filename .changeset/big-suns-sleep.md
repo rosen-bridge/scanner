1fc6decbeeaa24db6857ea36952ed1f199c694ff
fateme.r <fatemeh.r3366@gmail.com>
2025-01-06 11:56:36 +0330

    Update koios network to use the new `block_tx_cbor`
    Koios is deprecating the \tx_info api and \block_tx_info api accordingly.
    They are providing a new api \tx_cbor (and going to add \block_tx_cbor)
    that contains the serialized form of transaction instead of its object.

---
+++ .changeset/big-suns-sleep.md
@@ -0,0 +1,6 @@
+---
+'@rosen-bridge/observation-extractor': minor
+'@rosen-bridge/scanner': minor
+---
+
+Update koios scanner to use the new serialized APIs
--- packages/scanner/lib/scanner/cardano/interfaces/Koios.ts
+++ packages/scanner/lib/scanner/cardano/interfaces/Koios.ts
@@ -1,3 +1,5 @@
+import { TransactionJSON } from '@emurgo/cardano-serialization-lib-nodejs';
+
 interface KoiosBlock {
   hash: string;
   block_height: number;
@@ -12,77 +14,16 @@ interface KoiosBlockInfo {
   child_hash?: string;
 }
 
-interface PaymentAddr {
-  bech32: string;
-  cred: string;
-}
-
-interface Asset {
-  policy_id: string;
-  asset_name: string;
-  quantity: string;
-}
-
-interface UTXO {
-  payment_addr: PaymentAddr;
-  stake_addr?: string | null;
-  tx_hash: string;
-  tx_index: number;
-  value: string;
-  asset_list: Array<Asset>;
-}
-
-interface Withdrawal {
-  amount: string;
-  stake_addr: {
-    bech32: string;
-  };
-}
-
-interface MetaData {
-  [key: string]: Record<string, unknown>;
-}
-
-interface Certificate {
-  index?: number | null;
-  type: string;
-  info: Record<string, unknown>;
-}
-
-interface NativeScript {
-  script_hash: string;
-  script_json: Record<string, unknown>;
-}
-
-interface PlutusContract {
-  address: string;
-  script_hash: string;
-  bytecode: string;
-}
-
-interface KoiosTransaction {
+interface KoiosCborTx {
   tx_hash: string;
   block_hash: string;
   block_height: number;
   epoch_no: number;
-  epoch_slot: number;
+  absolute_slot: number;
   tx_timestamp: number;
-  tx_block_index: number;
-  tx_size: number;
-  total_output: string;
-  fee: string;
-  deposit: string;
-  invalid_before?: number | null;
-  invalid_after?: number | null;
-  collaterals: Array<UTXO>;
-  inputs: Array<UTXO>;
-  outputs: Array<UTXO>;
-  withdrawals: Array<Withdrawal>;
-  assets_minted: Array<Asset>;
-  metadata: MetaData;
-  certificates: Array<Certificate>;
-  native_scripts: Array<NativeScript>;
-  plutus_contracts: Array<PlutusContract>;
+  cbor: string;
 }
 
-export { KoiosBlock, KoiosBlockInfo, KoiosTransaction };
+type KoiosTransaction = KoiosCborTx & TransactionJSON;
+
+export { KoiosBlock, KoiosBlockInfo, KoiosTransaction, KoiosCborTx };
