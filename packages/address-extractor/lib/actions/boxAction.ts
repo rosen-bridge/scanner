9a933c1e2bb4307e25fc98eaff1bec27a619c79e
fateme.r <fatemeh.r3366@gmail.com>
2025-01-22 11:25:27 +0330

    Update address extractor to use the abstract ergo extractor entity and action

--- packages/address-extractor/lib/actions/boxAction.ts
+++ packages/address-extractor/lib/actions/boxAction.ts
@@ -1,196 +1,46 @@
-import { DataSource, In, Repository, Not } from 'typeorm';
-import { chunk, difference, pick } from 'lodash-es';
-import { AbstractLogger, DummyLogger } from '@rosen-bridge/abstract-logger';
+import { DataSource } from 'typeorm';
+import { pick } from 'lodash-es';
+import { AbstractLogger } from '@rosen-bridge/abstract-logger';
 import {
   AbstractInitializableErgoExtractorAction,
-  SpendInfo,
-  DB_CHUNK_SIZE,
   BlockInfo,
-  ErgoExtractedData,
 } from '@rosen-bridge/abstract-extractor';
 
 import { BoxEntity } from '../entities/boxEntity';
 import { ExtractedBox } from '../interfaces/types';
-import { JsonBI } from '../utils';
 
-export class BoxEntityAction extends AbstractInitializableErgoExtractorAction<ExtractedBox> {
-  private readonly datasource: DataSource;
-  readonly logger: AbstractLogger;
-  private readonly repository: Repository<BoxEntity>;
-
+export class BoxEntityAction extends AbstractInitializableErgoExtractorAction<
+  ExtractedBox,
+  BoxEntity
+> {
   constructor(dataSource: DataSource, logger?: AbstractLogger) {
-    super();
-    this.datasource = dataSource;
-    this.logger = logger ? logger : new DummyLogger();
-    this.repository = dataSource.getRepository(BoxEntity);
+    super(dataSource, BoxEntity, logger);
   }
 
   /**
-   * insert all extracted box data in an atomic transaction
-   * update the data if a box with the same id is already stored in db
-   * @param boxes
-   * @param block
-   * @param extractor
-   * @return inserted items and updated box ids
-   * returns undefined in case of any problem
+   * create the box entity from extracted data and block information
    */
-  insertBoxes = async (
-    boxes: Array<ExtractedBox>,
+  createEntity = (
+    boxes: ExtractedBox[],
     block: BlockInfo,
     extractor: string
-  ) => {
-    let success = true;
-    let boxesToInsert: ExtractedBox[] = [],
-      boxesToUpdate: ExtractedBox[] = [];
-    const queryRunner = this.datasource.createQueryRunner();
-    await queryRunner.connect();
-    await queryRunner.startTransaction();
-    const repository = await queryRunner.manager.getRepository(BoxEntity);
-    try {
-      const createEntities = (boxes: ExtractedBox[]) =>
-        boxes.map((box) => ({
-          address: box.address,
-          boxId: box.boxId,
-          createBlock: block.hash,
-          creationHeight: block.height,
-          serialized: box.serialized,
-          extractor: extractor,
-        }));
-
-      const dbBoxIds = (
-        await this.datasource.getRepository(BoxEntity).findBy({
-          boxId: In(boxes.map((item) => item.boxId)),
-          extractor: extractor,
-        })
-      ).map((box) => box.boxId);
-      if (dbBoxIds.length > 0)
-        this.logger.debug(`Found stored boxes with same boxId`, dbBoxIds);
-
-      boxesToUpdate = boxes.filter((box) => dbBoxIds.includes(box.boxId));
-      boxesToInsert = difference(boxes, boxesToUpdate);
-
-      if (boxesToInsert.length > 0) {
-        this.logger.debug(`Inserting boxes`);
-        await repository.insert(createEntities(boxesToInsert));
-      }
-      if (boxesToUpdate.length > 0)
-        this.logger.info(
-          `Updating boxes with following Ids in the database: [${boxesToUpdate
-            .map((col) => col.boxId)
-            .join(', ')}]`
-        );
-      createEntities(boxesToUpdate).forEach(async (boxEntity) => {
-        this.logger.debug(
-          `Updating boxes in database [${JsonBI.stringify(boxEntity)}]`
-        );
-        await repository.update(
-          { boxId: boxEntity.boxId, extractor: extractor },
-          boxEntity
-        );
-      });
-      await queryRunner.commitTransaction();
-    } catch (e) {
-      this.logger.error(`An error occurred during store boxes action: ${e}`);
-      await queryRunner.rollbackTransaction();
-      success = false;
-    } finally {
-      await queryRunner.release();
-    }
-    if (success)
-      return {
-        insertedData: boxesToInsert,
-        updatedData: boxesToUpdate.map((data) => pick(data, 'boxId')),
-      };
-    return undefined;
-  };
-
-  /**
-   * update spending information of stored boxes
-   * chunk spendInfos to prevent large database queries
-   * @param spendInfos
-   * @param block
-   * @param extractor
-   * @returns spent box ids
-   */
-  spendBoxes = async (
-    spendInfos: Array<SpendInfo>,
-    block: BlockInfo,
-    extractor: string
-  ): Promise<ErgoExtractedData[]> => {
-    const spentData = [];
-    const spendInfoChunks = chunk(spendInfos, DB_CHUNK_SIZE);
-    for (const spendInfoChunk of spendInfoChunks) {
-      const boxIds = spendInfoChunk.map((info) => info.boxId);
-      const updateResult = await this.repository.update(
-        { boxId: In(boxIds), extractor: extractor },
-        { spendBlock: block.hash, spendHeight: block.height }
-      );
-
-      if (updateResult.affected && updateResult.affected > 0) {
-        const spentRows = await this.repository.findBy({
-          boxId: In(boxIds),
-          spendBlock: block.hash,
-        });
-        spentData.push(...spentRows);
-        for (const row of spentRows) {
-          this.logger.debug(
-            `Spent box with boxId [${row.boxId}] at height ${block.height}`
-          );
-        }
-      }
-    }
-    return spentData.map((data) => pick(data, 'boxId'));
-  };
-
-  /**
-   * remove all existing data for the extractor
-   * @param extractorId
-   */
-  removeAllData = async (extractorId: string) => {
-    await this.repository.delete({ extractor: extractorId });
-  };
-
-  /**
-   * delete extracted data from a specific block
-   * if a box is spend in this block mark it as unspent
-   * if a box is created in this block remove it from database
-   * @param block
-   * @param extractorId
-   * @return deleted items and updated box ids
-   */
-  deleteBlockBoxes = async (block: string, extractor: string) => {
-    this.logger.info(
-      `Deleting boxes in block ${block} and extractor ${extractor}`
-    );
-    const deletedData = await this.repository.find({
-      where: { extractor: extractor, createBlock: block },
-    });
-    const updatedData = await this.repository.find({
-      where: {
-        extractor: extractor,
-        spendBlock: block,
-        createBlock: Not(block),
-      },
-    });
-    await this.repository.delete({
+  ): Omit<BoxEntity, 'id'>[] => {
+    return boxes.map((box) => ({
+      address: box.address,
+      boxId: box.boxId,
+      block: block.hash,
+      height: block.height,
+      serialized: box.serialized,
       extractor: extractor,
-      createBlock: block,
-    });
-    await this.repository.update(
-      { spendBlock: block, extractor: extractor },
-      { spendBlock: null, spendHeight: 0 }
+    }));
+  };
+
+  /**
+   * convert the database entity back to raw data
+   */
+  convertEntityToData = (entities: BoxEntity[]): ExtractedBox[] => {
+    return entities.map((data) =>
+      pick(data, ['boxId', 'address', 'serialized'])
     );
-    return {
-      deletedData: deletedData.map((data) =>
-        pick(data, [
-          'boxId',
-          'address',
-          'serialized',
-          'spendBlock',
-          'spendHeight',
-        ])
-      ),
-      updatedData: updatedData.map((data) => pick(data, 'boxId')),
-    };
   };
 }
