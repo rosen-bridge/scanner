a8a9f2fc15de608d5fa0780df7f159aa11fda756
mehran <mehran@protonmail.com>
2025-02-04 18:44:18 -0800

    Add rpc observation extractor

--- packages/observation-extractors/bitcoin-observation-extractor/lib/DogeRpcObservationExtractor.ts
+++ packages/observation-extractors/bitcoin-observation-extractor/lib/DogeRpcObservationExtractor.ts
@@ -1,12 +1,12 @@
 import { AbstractObservationExtractor } from '@rosen-bridge/observation-extractor';
-import { BitcoinRpcTransaction } from '@rosen-bridge/bitcoin-rpc-scanner';
-import { BitcoinRpcRosenExtractor } from '@rosen-bridge/rosen-extractor';
+import { DogeRpcTransaction } from '@rosen-bridge/bitcoin-rpc-scanner';
+import { DogeRpcRosenExtractor } from '@rosen-bridge/rosen-extractor';
 import { TokenMap } from '@rosen-bridge/tokens';
 import { AbstractLogger } from '@rosen-bridge/abstract-logger';
 import { DataSource } from 'typeorm';
 
-export class DogeRpcObservationExtractor extends AbstractObservationExtractor<BitcoinRpcTransaction> {
-  readonly FROM_CHAIN = 'bitcoin';
+export class DogeRpcObservationExtractor extends AbstractObservationExtractor<DogeRpcTransaction> {
+  readonly FROM_CHAIN = 'doge';
 
   constructor(
     lockAddress: string,
@@ -17,7 +17,7 @@ export class DogeRpcObservationExtractor extends AbstractObservationExtractor<Bi
     super(
       dataSource,
       tokens,
-      new BitcoinRpcRosenExtractor(lockAddress, tokens, logger),
+      new DogeRpcRosenExtractor(lockAddress, tokens, logger),
       logger
     );
   }
@@ -25,10 +25,10 @@ export class DogeRpcObservationExtractor extends AbstractObservationExtractor<Bi
   /**
    * gets Id for current extractor
    */
-  getId = () => 'bitcoin-rpc-extractor';
+  getId = () => 'doge-rpc-extractor';
 
   /**
    * gets transaction id from TransactionType
    */
-  getTxId = (tx: BitcoinRpcTransaction) => tx.txid;
+  getTxId = (tx: DogeRpcTransaction) => tx.txid;
 }
