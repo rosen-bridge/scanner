265346cab56f83b4b2792c320294972b68030280
ehsan gheychisaz <ehsan.ghechisaz82@gmail.com>
2025-08-13 04:29:13 +0330

    add ergo and cardano to scanners package

--- packages/abstract-scanner/lib/index.ts
+++ packages/abstract-scanner/lib/index.ts
@@ -1,8 +1,4 @@
 export { BlockDbAction } from './scanner/action';
-export { ErgoScanner } from './scanner/ergo/ergoScanner';
-export { CardanoKoiosScanner } from './scanner/cardano/scanner/koios';
-export { CardanoOgmiosScanner } from './scanner/cardano/scanner/ogmios';
-export { CardanoBlockFrostScanner } from './scanner/cardano/scanner/blockfrost';
 export { migrations } from './migrations';
 export { PROCEED } from './entities/blockEntity';
 export { BlockEntity } from './entities/blockEntity';
@@ -16,10 +12,4 @@ export {
   RoundRobinStrategy,
 } from './scanner/network/ConnectorSelectionStrategies';
 export { NetworkConnectorManager } from './scanner/network/NetworkConnectorManager';
-export { ErgoExplorerNetwork } from './scanner/ergo/network/ergoExplorerNetwork';
-export { ErgoNodeNetwork } from './scanner/ergo/network/ergoNodeNetwork';
-export { BlockFrostNetwork } from './scanner/cardano/network/blockfrost';
-export { KoiosNetwork } from './scanner/cardano/network/koios';
-export { BlockFrostTransaction } from './scanner/cardano/interfaces/BlockFrost';
-export { KoiosTransaction } from './scanner/cardano/interfaces/Koios';
 export { ScannerConfig } from './scanner/interfaces';
