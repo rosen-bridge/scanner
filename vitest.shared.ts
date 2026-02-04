c974c64e2f15852149aab7088a5b9569570b68d6
rasoul salehi <8raha6094@gmail.com>
2026-02-04 17:53:50 +0330

    Update vitest config to use shared config

---
+++ vitest.shared.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    coverage: {
+      all: true,
+      provider: 'istanbul',
+      reporter: 'cobertura',
+      reportsDirectory: './coverage',
+    },
+    poolOptions: {
+      forks: {
+        singleFork: true,
+      },
+    },
+  },
+});
