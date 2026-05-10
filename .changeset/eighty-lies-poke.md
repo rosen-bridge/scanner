a046f147f8681bad1e41456ba72f759075dfa440
HFazelinia <hadifazelinia78@gmail.com>
2026-05-10 12:45:58 +0100

    fix unisat pagination

---
+++ .changeset/eighty-lies-poke.md
@@ -0,0 +1,5 @@
+---
+'@rosen-bridge/bitcoin-runes-observation-extractor': patch
+---
+
+Fix bug where pagination was ignored while fetching address or transaction runes
--- packages/observation-extractors/bitcoin-runes-observation-extractor/tests/unisatRunesProtocolNetwork.spec.ts
+++ packages/observation-extractors/bitcoin-runes-observation-extractor/tests/unisatRunesProtocolNetwork.spec.ts
@@ -2,8 +2,11 @@ import { UnisatRunesProtocolNetwork } from '../lib/unisatRunesProtocolNetwork';
 import {
   mockTxId,
   mockUnisatResponse1,
-  mockUnisatResponse2,
+  mockUnisatResponse2Page1,
+  mockUnisatResponse2Page2,
   mockUnisatResponse3,
+  unisatResult,
+  userTxId,
 } from './testData';
 
 describe('UnisatRunesProtocolNetwork', () => {
@@ -40,25 +43,34 @@ describe('UnisatRunesProtocolNetwork', () => {
     });
 
     /**
-     * @target UnisatRunesProtocolNetwork.getTxOutputRunes should throw when the response does not match the request's pagination
+     * @target UnisatRunesProtocolNetwork.getTxOutputRunes should successfully get the transfers across multiple pages
      * @dependencies
      * @scenario
+     * - mock an extractor with low page size (3)
      * - stub unisatClient.get to resolve to a mock object
      * - call getTxOutputRunes using a mock tx id and block height
      * @expected
-     * - getTxOutputRunes should have thrown
+     * - getTxOutputRunes should have returned the mocked transfers
      */
-    it(`should throw when the response does not match the request's pagination`, async () => {
+    it(`should successfully get the transfers across multiple pages`, async () => {
+      const extractor = new UnisatRunesProtocolNetwork('', '');
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (extractor as any).PAGE_SIZE = 3;
       // arrange
       vi.spyOn(extractor['unisatClient'], 'get').mockResolvedValueOnce({
         code: 0,
-        data: { data: mockUnisatResponse2 },
+        data: { data: mockUnisatResponse2Page1 },
+      });
+      vi.spyOn(extractor['unisatClient'], 'get').mockResolvedValueOnce({
+        code: 0,
+        data: { data: mockUnisatResponse2Page2 },
       });
 
-      // act and assert
-      await expect(async () => {
-        await extractor.getTxOutputRunes(mockTxId, 100);
-      }).rejects.toThrow(Error);
+      // act
+      const result = await extractor.getTxOutputRunes(userTxId, 100);
+
+      // assert
+      expect(result).toEqual(unisatResult);
     });
 
     /**
